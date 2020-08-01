8e4fdff5a341078359232ac34e9bd96f82584253
Estevan Maito <ejmaito@gmail.com>
2020-08-01 13:18:55 -0300

    refactor nav

--- components/Nav.js
+++ components/Nav.js
@@ -3,24 +3,37 @@ import Link from 'next/link'
 
 function Nav() {
   return (
-    <nav className="max-w-2xl p-4 mx-auto">
-      <ul className="flex justify-end space-x-6 text-gray-900">
-        <li>
-          <Link href="/">
-            <a className="p-1">Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/blog">
-            <a className="p-1">Blog</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a className="p-1">About</a>
-          </Link>
-        </li>
-      </ul>
+    <nav className="max-w-2xl px-4 py-6 mx-auto text-gray-800 xs:px-8 lg:max-w-5xl">
+      <div className="flex justify-between -mx-4">
+        <Link href="/">
+          <a className="px-4 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-gray-50">
+            Estevan Maito
+          </a>
+        </Link>
+        <ul className="flex justify-end space-x-6">
+          <li>
+            <Link href="/">
+              <a className="px-4 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-gray-50">
+                Home
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+              <a className="px-4 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-gray-50">
+                Blog
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a className="px-4 py-2 transition-colors duration-100 ease-in-out rounded-md hover:bg-gray-50">
+                About
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
