64dbbf29713dc1c4b6ed22bca678373aed330ab3
Estevan Maito <ejmaito@gmail.com>
2020-08-03 18:35:45 -0300

    add case study

--- layouts/case.js
+++ layouts/case.js
@@ -1,50 +1,17 @@
-import Link from 'next/link'
 import Nav from '@/components/Nav'
 import SEO from '@/components/SEO'
-import { formatDate, formatPath } from '@/utils/format'
 
 export default function Post(frontMatter) {
   return ({ children: content }) => {
-    const canonicalURL = `https://estevanmaito.me/${formatPath(frontMatter.__resourcePath)}`
-    const socialImageURL = `https://estevanmaito.me/${formatPath(
-      frontMatter.__resourcePath,
-      '.png'
-    ).replace('blog', 'social')}`
+    const title = 'Case study: Unna Tech'
+    const description =
+      'A deep case study in a complete application, from design to server setup, how I put together every piece of technology.'
+    const canonical = 'https://estevanmaito.me/case/unna'
+    const image = 'https://estevanmaito.me/social-image.png'
 
     return (
       <>
-        {/* <SEO
-          title={`${frontMatter.title} - Estevan Maito`}
-          description={frontMatter.description}
-          canonical={canonicalURL}
-          image={socialImageURL}
-        >
-          <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{
-              __html: `{
-                "@context": "https://schema.org",
-                "@type": "BlogPosting",
-                "url": "${canonicalURL}",
-                "name": "${frontMatter.title} - Estevan Maito",
-                "description": "${frontMatter.description}",
-                "datePublished": "${frontMatter.datePublished}",
-                "image": {
-                  "@type": "ImageObject",
-                  "url": "${socialImageURL}"
-                },
-                "author": {
-                  "@type": "Person",
-                  "name": "Estevan Maito",
-                  "sameAs": [
-                    "https://twitter.com/estevanmaito",
-                    "https://github.com/estevanmaito"
-                  ]
-                }
-              }`,
-            }}
-          ></script>
-        </SEO> */}
+        <SEO title={title} description={description} canonical={canonical} image={image} />
         <Nav />
 
         <main className="max-w-2xl px-4 mx-auto my-10 prose xs:px-8">{content}</main>
