7fac89119a19529029aa07054e8aa4f433c1b0cb
Estevan Maito <ejmaito@gmail.com>
2020-08-05 23:17:27 -0300

    fix nav spacing

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 function Nav() {
   return (
     <nav className="max-w-2xl px-4 py-6 mx-auto text-gray-800 xs:px-8 lg:max-w-5xl">
-      <div className="flex items-center justify-between -mx-4 text-sm">
+      <div className="flex items-center justify-between -mx-2 text-sm md:-mx-4">
         <Link href="/">
           <a className="px-2 py-1 transition-colors duration-100 ease-in-out rounded-md sm:px-4 sm:py-2 hover:bg-gray-50">
             Estevan Maito
